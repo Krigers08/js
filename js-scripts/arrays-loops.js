1026783cd9d1e139cd9f7794482705e2079531da
Elina Viksne <elinaviksne124@gmail.com>
2024-11-05 08:35:47 +0200

    .\js-scripts\

---
+++ js-scripts/arrays-loops.js
@@ -0,0 +1,19 @@
+for (let i = 11; i <= 20; i++) {
+    console.log(i);
+  } 
+
+for (let i = 20; i >= 11; i--) {
+    console.log(i);
+  } 
+
+let names = ["Sigma" , "Ligma" , "Nigma" , "Enigma" , "Eligma"]
+console.log(names[0])
+console.log(names[2])
+
+let length = names.length
+console.log(names.length)
+
+for(let i = 0; i < names.length; i++){
+    console.log(i);
+    console.log(names[i]);
+    }
---
+++ js-scripts/variables.js
@@ -0,0 +1,8 @@
+let name = "Krigga";
+let age = 16;
+let isStudent = false;
+let PI = 3.14
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(PI)
