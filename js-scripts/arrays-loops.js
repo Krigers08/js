86c70ce2cfaeabe3b6ff01b31d9f7700407ba225
Kristers <you@example.com>
2024-11-07 13:25:38 +0200

    07.11.2024 klases darbs

--- js-scripts/arrays-loops.js
+++ js-scripts/arrays-loops.js
@@ -14,6 +14,5 @@ let length = names.length
 console.log(names.length)
 
 for(let i = 0; i < names.length; i++){
-    console.log(i);
-    console.log(names[i]);
+    console.log(`${i + 1}. ${names[i]}`);
     }
---
+++ js-scripts/objekti.js
@@ -0,0 +1,23 @@
+let person{
+let name = "Jānis";
+let age = 16;
+let isStudent = false;
+const PI = 3.14;
+}
+console.log(name)
+
+if(person.age >= 18){
+    console.log = "Vecs"
+}else{
+    console.log = "Jauns"
+}
+person.age(16)
+
+function printName(firstName){
+    console.log(firstName)
+}
+printName("Krigga")
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  } 
